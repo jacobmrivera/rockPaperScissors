5dc98ee59c67e0dee55ebc24d1c1fee8246233c4
Jacob Rivera <jakemichaelrivera32@gmail.com>
2022-07-22 11:36:30 -0500

    Create playRound function to play one round of Rock, Paper, Scissors, which returns a string statement of the result of the round

--- index.js
+++ index.js
@@ -11,3 +11,41 @@ function getComputerChoice(){
         console.log("Something aint workin")
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    let state = "tie";
+
+    if (playerSelection == "rock") {
+        if (computerSelection == "Paper") {
+            state = "lose";
+        } else if (computerSelection == "Scissors") {
+            state = "win";
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "Scissors") {
+            state = "lose";
+        } else if (computerSelection == "Rock") {
+            state = "win";
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "Rock") {
+            state = "lose";
+        } else if (computerSelection == "Paper") {
+            state = "win";
+        }
+    }
+
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
+    if (state == "win") {
+        return `You win!  ${playerSelection} beats ${computerSelection}.`;
+    } else if (state == "lose") {
+        return `You lose! ${computerSelection} beats  ${playerSelection}.`;
+    }else {
+        return "You tie!";
+    }
+
+}
